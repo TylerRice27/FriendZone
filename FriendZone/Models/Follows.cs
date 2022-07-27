8fc8c4a61a636624be26821d682a6381b0bf26b2
TylerRice27 <Tylerrayrice2@gmail.com>
2022-07-27 14:10:17 -0600

    Model

---
+++ FriendZone/Models/Follows.cs
@@ -0,0 +1,12 @@
+namespace FriendZone.Models
+{
+    public class Follows
+    {
+        public int Id { get; set; }
+
+        public string FollowerId { get; set; }
+
+        public string FollowingId { get; set; }
+
+    }
+}
